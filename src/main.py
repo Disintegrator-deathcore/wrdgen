7eacb0c79581d3e1e698e90b5cb3d4687a6aedde
Disintero <dychko2004@gmail.com>
2026-01-17 15:57:43 +0500

    Оптимизирована функция remorph_word

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from docx import Document
 from python_docx_replace import docx_replace, docx_get_keys
 import pymorphy3
+from openpyxl import load_workbook
 
 
 class MainApp():
@@ -8,27 +9,27 @@ class MainApp():
         # Вводные данные
         self.morph = pymorphy3.MorphAnalyzer()
         self.target_text = [
-            "{{ learning_first }}",
-            "{{ curs_num }}",
-            "{{ group_name }}",
-            "{{ name_ro }}",
-            "{{ practice_start_fd }} "
-            "{{ practice_end_fd }}",
-            "{{ learning_sec }}",
-            "{{ name_im }}",
-            "{{ order_num }}",
-            "{{ order_date }}",
-            "{{ P_num }}",
-            "{{ specialization }}",
-            "{{ PM_name }}",
-            "{{ practice_start_day }}",
-            "{{ practice_start_month }}",
-            "{{ practice_start_year}}",
-            "{{ practice_end_day }}",
-            "{{ practice_end_month }}",
-            "{{ practice_start_year }}",
-            "{{ hours }}",
-            "{{ end_hour }}"
+            "learning_first",
+            "curs_num",
+            "group_name",
+            "name_ro",
+            "practice_start_fd  "
+            "practice_end_fd",
+            "learning_sec",
+            "name_im",
+            "order_num",
+            "order_date",
+            "P_num",
+            "specialization",
+            "PM_name",
+            "practice_start_day",
+            "practice_start_month",
+            "practice_start_year",
+            "practice_end_day",
+            "practice_end_month",
+            "practice_start_year",
+            "hours",
+            "end_hour"
         ]
         self.doc_name = doc_name
         
@@ -51,8 +52,9 @@ class MainApp():
     def replace_text(self):
         ########
         students = [
-            {"name":"Иванов Иван Иванович"},
-            {"name":"Андреев Андрей Андреевич"}
+            "Иванов Иван Иванович",
+            "Андреев Андрей Андреевич",
+            "Авдиенко Ирина Дмитриевна"
         ]
         
         data_dict = {
@@ -67,27 +69,35 @@ class MainApp():
         
         # Заменяем текст в документе для каждого студента
         for student in students:
-            data_dict["name_im"] = student["name"]
-            data_dict["name_ro"] = self.remorph_word(student["name"])
+            data_dict["name_im"] = student
+            data_dict["name_ro"] = self.remorph_word(student)
             
             
             template = Document(self.doc_name)
             
             docx_replace(template, **data_dict)
             
-            template.save(f"{student['name'].split()[0]}.docx")
+            template.save(f"results/{student.split()[0]}.docx")
             
     def remorph_word(self, word):
-        full_name = ""
+        transformed_words = []
+        for part in word.split():
+            try:
+                # Получаем первую интерпретацию слова и преобразуем её в родительный падеж
+                parsed_word = self.morph.parse(part)[0].inflect({'gent'})
+                
+                # Преобразуем слово в строчную форму и делаем первую букву заглавной
+                if parsed_word:
+                    inflected_word = parsed_word.word.capitalize()
+                    transformed_words.append(inflected_word)
             
-        for p_name in word.split():
-            ch_word = self.morph.parse(p_name)[0].inflect({"gent"})[0]
-            ch_word = ch_word[0].upper() + ch_word[1:]
-            full_name += f"{ch_word} "
-        
-        full_name = full_name.strip()
-        
-        return full_name
+            except Exception as e:
+                print(f"Произошла ошибка при обработке слова '{part}': {e}")
+
+        return ' '.join(transformed_words)
+    
+    def students_update(self):
+        pass
     
 if __name__ == "__main__":
     MainApp()
