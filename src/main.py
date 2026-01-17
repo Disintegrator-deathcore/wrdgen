567760d309c34b24ec5a2b89f329ce7e78a32bc1
Disintero <dychko2004@gmail.com>
2026-01-17 12:39:31 +0500

    Доработан функционал замены и преобразования текста

--- src/main.py
+++ src/main.py
@@ -1,12 +1,42 @@
 from docx import Document
-from docx.shared import Pt
+from python_docx_replace import docx_replace, docx_get_keys
+import pymorphy3
 
 
 class MainApp():
-    def __init__(self):
+    def __init__(self, doc_name="./test_data/template.docx"):
+        # Вводные данные
+        self.morph = pymorphy3.MorphAnalyzer()
+        self.target_text = [
+            "{{ learning_first }}",
+            "{{ curs_num }}",
+            "{{ group_name }}",
+            "{{ name_ro }}",
+            "{{ practice_start_fd }} "
+            "{{ practice_end_fd }}",
+            "{{ learning_sec }}",
+            "{{ name_im }}",
+            "{{ order_num }}",
+            "{{ order_date }}",
+            "{{ P_num }}",
+            "{{ specialization }}",
+            "{{ PM_name }}",
+            "{{ practice_start_day }}",
+            "{{ practice_start_month }}",
+            "{{ practice_start_year}}",
+            "{{ practice_end_day }}",
+            "{{ practice_end_month }}",
+            "{{ practice_start_year }}",
+            "{{ hours }}",
+            "{{ end_hour }}"
+        ]
+        self.doc_name = doc_name
+        
+        # Вызовы функций
         # self.get_list()
-        self.open_doc()
+        self.replace_text()
     
+    # Ввод пользователем списка студентов
     def get_list(self):
         stud_list = []
         
@@ -17,30 +47,47 @@ class MainApp():
             
             stud_list.append(stdin)
             
-    def open_doc(self, doc_name="./test_data/template.docx"):
+    # Функция заены текста в документе
+    def replace_text(self):
+        ########
         students = [
             {"name":"Иванов Иван Иванович"},
             {"name":"Андреев Андрей Андреевич"}
         ]
         
-        target_text = "{{ name_im }}"
+        data_dict = {
+            "learning_first": "Обучающегося",
+            "curs_num": "2",
+            "group_name": "170 ис",
+            "name_im": "Иванов Иван Иванович",
+            "name_ro": "Иванова Ивана Ивановича"
+        }
+        ########
         
+        
+        # Заменяем текст в документе для каждого студента
         for student in students:
-            template = Document(doc_name)
-        
-            print(f"Документ открыт, и используется студент: {student['name']}")
-            for paragraph in template.paragraphs:
-                if target_text in paragraph.text:
-                    paragraph.text = paragraph.text.replace(target_text, " ")
-                    new_text = paragraph.add_run(student['name'])
-                    print(f"поэтому мы заполняем {student['name']}")
-                    
-                    new_text.font.name = "Times New Roman"
-                    new_text.font.size = Pt(20)
-                    new_text.bold = True
-                    new_text.underline = True
+            data_dict["name_im"] = student["name"]
+            data_dict["name_ro"] = self.remorph_word(student["name"])
+            
+            
+            template = Document(self.doc_name)
+            
+            docx_replace(template, **data_dict)
             
             template.save(f"{student['name'].split()[0]}.docx")
+            
+    def remorph_word(self, word):
+        full_name = ""
+            
+        for p_name in word.split():
+            ch_word = self.morph.parse(p_name)[0].inflect({"gent"})[0]
+            ch_word = ch_word[0].upper() + ch_word[1:]
+            full_name += f"{ch_word} "
+        
+        full_name = full_name.strip()
+        
+        return full_name
     
 if __name__ == "__main__":
     MainApp()
