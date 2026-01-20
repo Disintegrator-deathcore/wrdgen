a800e31b950ff536459eadeb4049bbdfd8df9e26
Disintero <dychko2004@gmail.com>
2026-01-20 11:00:34 +0500

    Произведены улучшения по лаконичности кода и минимизации затрат памяти при работе программы

--- src/main.py
+++ src/main.py
@@ -6,52 +6,36 @@ from openpyxl import load_workbook
 
 class MainApp():
     def __init__(self, doc_name="./test_data/template.docx"):
-        # Вводные данные
         self.morph = pymorphy3.MorphAnalyzer()
         self.target_text = {
-            "learning_first":"",
-            "curs_num":"",
-            "group_name":"",
-            "name_ro":"",
-            "practice_start_fd":"",
-            "practice_end_fd":"",
-            "learning_sec":"",
-            "name_im":"",
-            "order_num":"",
-            "order_date":"",
-            "P_num":"",
-            "specialization":"",
-            "PM_name":"",
-            "practice_start_day":"",
-            "practice_start_month":"",
-            "practice_start_year":"",
-            "practice_end_day":"",
-            "practice_end_month":"",
-            "practice_start_year":"",
-            "hours":"",
-            "end_hour":"",
-            "gender":""
+            "learning_first": "",
+            "curs_num": "",
+            "group_name": "",
+            "name_ro": "",
+            "practice_start_fd": "",
+            "practice_end_fd": "",
+            "learning_sec": "",
+            "name_im": "",
+            "order_num": "",
+            "order_date": "",
+            "P_num": "",
+            "specialization": "",
+            "PM_name": "",
+            "practice_start_day": "",
+            "practice_start_month": "",
+            "practice_start_year": "",
+            "practice_end_day": "",
+            "practice_end_month": "",
+            "practice_start_year": "",
+            "hours": "",
+            "end_hour": "",
+            "gender": ""
         }
-        # self.students_update()
         self.doc_name = doc_name
-        
-        # Вызовы функций
         self.data_update()
         self.replace_text(self.students_update())
             
-    # Функция заены текста в документе
     def replace_text(self, students):
-        # ########
-        
-        # self.target_text = {
-        #     "learning_first": "Обучающегося",
-        #     "curs_num": "2",
-        #     "group_name": "170 ис",
-        #     "learning_sec": "Обучающийся"
-        # }
-        # ########
-
-        # Заменяем текст в документе для каждого студента
         for student in students:
             self.target_text["name_im"] = student
             self.target_text["name_ro"] = self.remorph_word(student)
@@ -65,88 +49,74 @@ class MainApp():
             docx_replace(template, **self.target_text)
             
             # Сохраняем изменённый документ с новым названием
-            template.save(f"results/{self.target_text["name_ro"].split()[0]}.docx")
+            template.save(f"results/{self.target_text['name_ro'].split()[0]}.docx")
             
+            # Закрываем документ (хотя в python-docx явное закрытие не требуется, но всё равно освобождаем ресурс)
+            del template
+    
     def remorph_word(self, word):
         if len(word.split()) > 1:
-            transformed_words = []
-            for part in word.split():
-                try:    
-                    if part != self.morph.parse(part)[0].normal_form:
-                        pass
-                
-                    # Получаем первую интерпретацию слова и преобразуем её в родительный падеж
-                    parsed_word = self.morph.parse(part)[0].inflect({'gent'})
-                    
-                    # Преобразуем слово в строчную форму и делаем первую букву заглавной
-                    if parsed_word:
-                        inflected_word = parsed_word.word.capitalize()
-                        transformed_words.append(inflected_word)
-                        self.target_text["gender"] = self.morph.parse(part)[0].tag.gender
-                        
-                    elif parsed_word == None:
-                        transformed_words.append(part)
-                        
-
-                except Exception as e:
-                    print(f"Произошла ошибка при обработке слова '{part}': {e}")
-            return ' '.join(transformed_words)
-        
+            return self.fn_remorph(word)
         else:
-            try:
-                if self.target_text["gender"] == "femn":
-                    word = self.morph.parse(word)[0].inflect({"femn"}).word.capitalize()
-                else:
-                    word = self.morph.parse(word)[0].inflect({"masc"}).word.capitalize()
-                
-            except Exception as e:
-                print(f"Произошла ошибка при обработке слова '{word}: {e}")
-                
+            return self.learning_remorph(word)
+    
+    # Преобразование одного слова
+    def learning_remorph(self, word):
+        try:
+            parse_result = self.morph.parse(word)[0]
+            gender_tag = {"femn" if self.target_text["gender"] == "femn" else "masc"}
+            new_word = parse_result.inflect(gender_tag).word.capitalize()
+            return new_word
+        except Exception as e:
+            print(f"Ошибка при обработке слова '{word}': {e}")
             return word
     
-    def data_update(self, data_file="test_data/list_students.xlsx"):
-        workbok = load_workbook(filename=data_file)
-        
-        sheet = workbok.active
-        
-        self.target_text["learning_first"] = sheet["B1"].value
-        self.target_text["curs_num"] = sheet["C1"].value
-        self.target_text["group_name"] = sheet["D1"].value
-        self.target_text["practice_start_fd"] = sheet["E1"].value
-        self.target_text["practice_end_fd"] = sheet["F1"].value
-        self.target_text["learning_sec"] = sheet["G1"].value
-        self.target_text["order_num"] = sheet["H1"].value
-        self.target_text["order_date"] = sheet["I1"].value
-        self.target_text["P_num"] = sheet["J1"].value
-        self.target_text["specialization"] = sheet["K1"].value
-        self.target_text["PM_name"] = sheet["L1"].value
-        self.target_text["practice_start_day"] = sheet["M1"].value
-        self.target_text["practice_start_month"] = sheet["N1"].value
-        self.target_text["practice_start_year"] = sheet["O1"].value
-        self.target_text["practice_end_day"] = sheet["P1"].value
-        self.target_text["practice_end_month"] = sheet["Q1"].value
-        self.target_text["hours"] = sheet["R1"].value
-        self.target_text["end_hour"] = sheet["S1"].value
-        
+    # Преобразование ФИО в дательный падеж
+    def fn_remorph(self, full_name):
+        parts = full_name.split()
+        result_parts = []
+        for part in parts:
+            try:
+                # Парсим и превращаем в родительный падеж
+                inflected_part = self.morph.parse(part)[0].inflect({'gent'}).word.capitalize()
+                result_parts.append(inflected_part)
+            except Exception as e:
+                print(f"Ошибка при обработке имени '{part}': {e}")
+                result_parts.append(part)
+        return ' '.join(result_parts)
     
-    def students_update(self, data_file="test_data/list_students.xlsx"):
-        # Загружаем рабочую книгу
-        workbook = load_workbook(filename=data_file)
-        
-        # Выбираем активный лист (обычно первый лист файла)
+    # Функция обновления данных в словаре из excel
+    def data_update(self, data_file="test_data/list_students.xlsx"):
+        workbook = load_workbook(filename=data_file, read_only=True)
         sheet = workbook.active
         
-        student_list = []
+        # Забираем конкретные значения из определённых ячеек
+        self.target_text.update({
+            "learning_first": sheet["B1"].value,
+            "curs_num": sheet["C1"].value,
+            "group_name": sheet["D1"].value,
+            "practice_start_fd": sheet["E1"].value,
+            "practice_end_fd": sheet["F1"].value,
+            "learning_sec": sheet["G1"].value,
+            "order_num": sheet["H1"].value,
+            "order_date": sheet["I1"].value,
+            "P_num": sheet["J1"].value,
+            "specialization": sheet["K1"].value,
+            "PM_name": sheet["L1"].value,
+            "practice_start_day": sheet["M1"].value,
+            "practice_start_month": sheet["N1"].value,
+            "practice_start_year": sheet["O1"].value,
+            "practice_end_day": sheet["P1"].value,
+            "practice_end_month": sheet["Q1"].value,
+            "hours": sheet["R1"].value,
+            "end_hour": sheet["S1"].value
+        })
         
-        # Проходим по первому столбцу (столбец 'A') начиная с первой строки
-        for row in sheet.iter_rows(min_row=1, max_col=1, values_only=True):
-            cell_value = row[0]
-            
-            # Проверяем наличие значения в ячейке
-            if cell_value is not None:
-                student_list.append(cell_value.strip())
-        
-        return student_list
+    # Функция обновления списка студентов из excel
+    def students_update(self, data_file="test_data/list_students.xlsx"):
+        workbook = load_workbook(filename=data_file, read_only=True)
+        sheet = workbook.active
+        return [row[0].strip() for row in sheet.iter_rows(min_row=1, max_col=1, values_only=True) if row[0]]
     
 if __name__ == "__main__":
     MainApp()
