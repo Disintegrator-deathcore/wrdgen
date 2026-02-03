0f0c7e056e88872150b482c36ee06628d24c7abb
Disintero <dychko2004@gmail.com>
2026-02-03 11:30:12 +0500

    Добавлены комментарии, исправлены отступы

--- src/gui.py
+++ src/gui.py
@@ -3,7 +3,7 @@ import toga
 import asyncio
 import os
 
-from main import MainApp  # Предположительно здесь лежит ваш класс MainApp
+from main import MainApp
 
 
 class MyApp(toga.App):
@@ -11,10 +11,12 @@ class MyApp(toga.App):
         self.template = ""
         self.data = ""
         
+        # Создание основного окна
         self.main_window = toga.MainWindow(resizable=False)
         self.main_window.size = (550, 300)
         self.main_window.content = toga.Box()
         
+        # Создание областей для надписей и кнопок
         self.labels_box = toga.Box(direction="column", align_items="start", width=300)
         self.buttons_box = toga.Box(direction="column", align_items="start")
         
@@ -55,9 +57,11 @@ class MyApp(toga.App):
         self.start_btn.style.font_family = "Times New Roman"
         self.start_btn.style.font_size = 16
         
+        # Создание индикатора загрузки
         self.load_indc = toga.ActivityIndicator()
         self.load_indc.style.margin = (135, 200, -200, -275)
         
+        # Помещение всех элементов в нудные области
         self.buttons_box.add(
             self.select_template_btn,
             self.select_data_btn
@@ -76,49 +80,53 @@ class MyApp(toga.App):
         )
         
         self.main_window.show()
-        
+    
+    # Обработка запуска скрипта
     def start_script(self, widget):
-        """Обработчик нажатия кнопки"""
+        # Обработка события выбора файлов
         if self.template != "":
             if self.data != "":
                 self.load_indc.start()
-                thread = threading.Thread(target=self.run_long_task)
+                thread = threading.Thread(target=self.start_gen)
                 thread.start()
             else:
                 self.info()
         else:
             self.info()
-            print("Функция выполнена")
-        
     
+    # Запуск скрипта
+    def start_gen(self):
+        MainApp(self.template, self.data)
+        self.load_indc.stop()
+    
+    # Функция выбора шаблона
     def select_template(self, widget):
         cur_template = toga.OpenFileDialog("Выберите файл", str(os.getcwd()), ["docx", "doc"], False)
         
         task = asyncio.create_task(self.main_window.dialog(cur_template))
         task.add_done_callback(self.dialog_dismissed_template)
     
+    # Ответная функция выбора шаблона
+    def dialog_dismissed_template(self, task):
+        self.template_file_txtInp.value = task.result()
+        self.template = task.result()
+    
+    # Функция выбора файла с данными
     def select_data(self, widget):
         cur_data = toga.OpenFileDialog("Выберите файл", str(os.getcwd()), ["xlsx", "xls"], False)
         
         task = asyncio.create_task(self.main_window.dialog(cur_data))
         task.add_done_callback(self.dialog_dismissed_data)
     
-    def dialog_dismissed_template(self, task):
-        self.template_file_txtInp.value = task.result()
-        self.template = task.result()
-    
+    # Ответная функция выбора файла с данными
     def dialog_dismissed_data(self, task):
         self.data_file_txtInp.value = task.result()
         self.data = task.result()
-        
+    
+    # Отображение информации пользователю о выборе файла
     def info(self, **kwargs):
         my_info = toga.InfoDialog("Забыли выбрать", "Вы забыли выбрать файл")
         task = asyncio.create_task(self.main_window.dialog(my_info))
-    
-    def run_long_task(self):
-        """Функция, содержащая длительную задачу."""
-        MainApp(self.template, self.data)
-        self.load_indc.stop()
 
 
 if __name__ == '__main__':
